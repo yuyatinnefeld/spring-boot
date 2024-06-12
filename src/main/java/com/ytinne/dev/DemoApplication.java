090f107fbf6661fcd6bd066f7a611f454c970b24
yuyatinnefeld <yuyawamo@gmail.com>
2024-06-12 10:21:44 +0200

    refactor: relocate spring boot src files

--- src/main/java/com/ytinne/dev/DemoApplication.java
+++ src/main/java/com/ytinne/dev/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.ytinne.demo;
+package com.ytinne.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/ytinne/dev/HelloController.java
+++ src/main/java/com/ytinne/dev/HelloController.java
@@ -1,4 +1,4 @@
-package com.ytinne.demo;
+package com.ytinne.dev;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
