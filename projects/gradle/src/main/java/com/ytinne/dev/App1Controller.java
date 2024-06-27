ead0aa1c70f97b5923bd93cb6efc0d20a509b38e
yuyatinnefeld <yuyawamo@gmail.com>
2024-06-27 13:40:29 +0200

    feat: update app1 endpoint with json obj

--- projects/gradle/src/main/java/com/ytinne/dev/App1Controller.java
+++ projects/gradle/src/main/java/com/ytinne/dev/App1Controller.java
@@ -2,23 +2,27 @@ package com.ytinne.dev;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
 public class App1Controller {
 
     @GetMapping("/")
     public String mainPage() {
-		return "Greetings from Spring Boot!";
+        return "Greetings from Spring Boot!";
     }
-  
+
     @GetMapping("/app1")
-    public String callApp1Service() {
-		return "Greetings from App1";
+    @ResponseBody
+    public Map<String, String> callApp1Service() {
+        return Collections.singletonMap("app_name", "app1");
     }
 
     @GetMapping("/build")
     public String buildInfo() {
-		return "Build with Gradle";
+        return "Build with Gradle";
     }
-
 }
+
