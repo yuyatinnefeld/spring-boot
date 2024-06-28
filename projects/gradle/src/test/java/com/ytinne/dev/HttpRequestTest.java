b7a16c40cf9c047a1c64837ae60538ad4634e1ed
yuyatinnefeld <yuyawamo@gmail.com>
2024-06-28 09:42:33 +0200

    feat: update gradle test

--- projects/gradle/src/test/java/com/ytinne/dev/HttpRequestTest.java
+++ projects/gradle/src/test/java/com/ytinne/dev/HttpRequestTest.java
@@ -8,8 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.web.server.LocalServerPort;
-
+import org.springframework.http.ResponseEntity;
 import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class HttpRequestTest {
@@ -25,4 +26,12 @@ class HttpRequestTest {
 		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
 				String.class)).contains("Greetings");
 	}
+
+    @Test
+    void app1ShouldReturnAppName() throws Exception {
+        ResponseEntity<String> response = this.restTemplate.getForEntity("http://localhost:" + port + "/app1", String.class);
+        assertThat(response.getStatusCodeValue()).isEqualTo(200);
+        assertThat(response.getBody()).isEqualTo("{\"app_name\":\"app1\"}");
+    }
+
 }
